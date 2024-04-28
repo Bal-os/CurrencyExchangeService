ce4199810cd2633ab16a25d047bf86aa474e006e
Bal-os <sbal.stels@gmail.com>
2024-04-28 15:01:03 +0300

    Update: add prod configs.

--- src/main/java/os/balashov/currencyexchangeservice/infrastructure/provider/nbu/NbuClient.java
+++ src/main/java/os/balashov/currencyexchangeservice/infrastructure/provider/nbu/NbuClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Component
-@Profile("dev")
+@Profile("dev,prod")
 @FeignClient(name = "nbu-client", url = "${nbu.api.url}", configuration = FeignConfig.class)
 public interface NbuClient {
 
